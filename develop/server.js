ec31ebdcd567a83bdcb1a393cc547c9391ca8828
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-09-02 13:14:39 -0400

    API routes file structure

--- develop/server.js
+++ develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const path = require ('path');
 
 
 const app = express();
@@ -7,6 +8,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname,"public")));
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
