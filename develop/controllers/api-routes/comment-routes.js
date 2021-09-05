a998b7b6a5d16efcc67dfd80e7b1bfbba417d8b1
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-09-05 13:58:48 -0400

    Forgot to Save

--- develop/controllers/api-routes/comment-routes.js
+++ develop/controllers/api-routes/comment-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User, Post, Comment } = require("../../models/model-loader");
 
 //Get all Comments
-router.get("/", async (req,res) => {
+router.get("/", async (req, res) => {
     const data = await Comment.findAll({
         attributes: { exclude: ["password"] }
     });
@@ -10,32 +10,32 @@ router.get("/", async (req,res) => {
 });
 
 //Get Comment by Id
-router.get("/:id", async (req,res) => {
+router.get("/:id", async (req, res) => {
     const data = await Comment.findByPk(req.params.id, {
         attributes: { exclude: ['password'] },
-        include: [ { model: User}, {model: Post} ]
-});
+        include: [{ model: User }, { model: Post }]
+    });
     res.json(data);
 });
 
 //Create Comment
-router.post("/", async (req,res) => {
+router.post("/", async (req, res) => {
     const data = await Comment.create(req.body);
     res.json(data);
 });
 
 //Edit Comment by Id
-router.put("/:id", async (req,res) => {
+router.put("/:id", async (req, res) => {
     const data = await Comment.update(req.body, {
-        where: {id: req.params.id}
+        where: { id: req.params.id }
     })
     res.jsonp(data);
 });
 
 //Delete Comment by Id
-router.delete("/:id", async (req,res) => {
+router.delete("/:id", async (req, res) => {
     const data = await Comment.destroy({
-        where: {id: req.params.id}
+        where: { id: req.params.id }
     })
     res.json(data);
 });
