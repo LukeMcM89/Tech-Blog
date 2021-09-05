e1ea352793265c7fa6e396b0e3863cab32eb0200
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-09-05 13:02:09 -0400

    Post API routes additions

--- develop/controllers/api-routes/post-routes.js
+++ develop/controllers/api-routes/post-routes.js
@@ -7,6 +7,7 @@ router.get("/", async (req,res) => {
         include: [{model: User}, {model: Comment}],
         attributes: { model: User['name']}
     })
+    res.json(data);
 });
 
 //Get Post by Id
@@ -14,39 +15,33 @@ router.get("/:id", async (req,res) => {
     const data = await Post.findByPk(req.params.id, {
     attributes: {model: User ['name']},
     include: [{model: User}]
-    
+    })
+    res.json(data);
 });
 
 //Create Post
-router.post("/",(req,res) => {
+router.post("/", async (req,res) => {
+    const data = await Post.create(req.body);
+    res.json(data);
     
 });
 
 //Edit Post by Id
-router.put("/:id",(req,res) => {
-    
+router.put("/:id", async (req,res) => {
+    const data = await Post.update(req.body, {
+        where: {id: req.params.id}
+    })
+    res.json(data);
 });
 
 //Delete Post by Id
-router.delete("/:id",(req,res) => {
+router.delete("/:id", async (req,res) => {
+    const data = await Post.destroy({
+        where: {id: req.params.id}
+    })
+    res.json(data);
     
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 module.exports = router;
